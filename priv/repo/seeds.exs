fa8c44d84994183a19e5a9f779f22c98d302f27f
Jorge Valdez <valdez.a.jorge@gmail.com>
2019-02-23 16:52:28 -0600

    added seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Jamstack.JS.Party
+
+Jamstack.Repo.insert!(
+  %Party{
+      title: "Best Party",
+      owner_id: "bobnisco",
+      join_code: "meme-boys"
+  }
+)
+
+Jamstack.Repo.insert!(
+  %Party{
+    title: "Cool Party 2",
+    owner_id: "1212204105",
+    join_code: "luck-meal"
+  }
+)
+
+Jamstack.Repo.insert!(
+  %Party{
+    title: "meme land 7",
+    owner_id: "12130660101",
+    join_code: "doge-wine"
+  }
+)
